7df89ab1fd640436a0edf8c5a7e282cac6f23f9b
damian-anslik <damiananslik@outlook.com>
2024-03-24 22:42:01 +0000

    remove docs page, redirect to index

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,3 +10,8 @@ from .routers import reports
 app = FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(reports.reports_router, tags=["reports"])
+
+
+@app.exception_handler(404)
+async def http_exception_handler(_, __):
+    return RedirectResponse("/")
