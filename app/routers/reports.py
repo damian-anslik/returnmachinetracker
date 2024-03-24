32a0d26a83d7dcd320408a118766d9342f07a448
damian-anslik <damiananslik@outlook.com>
2024-03-24 23:08:04 +0000

    add limit to number reports, track report by user

--- app/routers/reports.py
+++ app/routers/reports.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, JSONResponse
+from uuid import uuid4
 
 from ..services import reports as reports_service
 from ..services import locations as locations_service
@@ -10,12 +12,15 @@ templates = Jinja2Templates(directory="./app/templates")
 
 @reports_router.get("/")
 async def get_home(request: Request):
-    return templates.TemplateResponse(
+    session_id = request.cookies.get("session_id", str(uuid4()))
+    response = templates.TemplateResponse(
         "index.html",
         {
             "request": request,
         },
     )
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 @reports_router.get("/reports")
@@ -35,15 +40,25 @@ async def get_locations():
 
 
 @reports_router.post("/reports")
-async def create_report(location_id: str):
+async def create_report(request: Request, location_id: str):
+    session_id = request.cookies.get("session_id", str(uuid4()))
     try:
-        reports_service.create_report(location_id)
+        reports_service.create_report(location_id, session_id)
         location_reports = reports_service.get_reports_by_location(
             location_id=location_id, num_days=1
         )
-        return {"reports": location_reports}
+        response = JSONResponse(
+            content={"reports": location_reports},
+        )
+        response.set_cookie("session_id", session_id)
+        return response
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=str(e),
         )
