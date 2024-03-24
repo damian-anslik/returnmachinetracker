a8333549b6a0baf44a9f32a10e49cc5f798caaa4
damian-anslik <damiananslik@outlook.com>
2024-03-24 22:18:07 +0000

    add location data, do not show docs page

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 from .routers import reports
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(reports.reports_router, tags=["reports"])
